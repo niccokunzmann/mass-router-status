1f42f6afd5c8a640d8991a289af10e43b41c1101
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-11-11 05:58:54 +0100

    output ips in order

--- check-router.sh
+++ check-router.sh
@@ -3,19 +3,6 @@ cd "`dirname \"$0\"`"
 
 ip="$1"
 
-# color codes from https://stackoverflow.com/a/5947802
-RED='\033[0;31m'
-GREEN='\033[1;32m'
-NC='\033[0m' # No Color
-
-function test-router() {
-    ping -c 1 "$ip" 1>&2
-}
-
-if test-router; then
-    echo -en "${GREEN}$ip${NC} "
-else
-    echo -en "${RED}$ip${NC} "
-fi
+ping -c 1 "$ip" 1>&2
 
 
--- show-status.sh
+++ show-status.sh
@@ -2,14 +2,37 @@
 
 cd "`dirname \"$0\"`"
 
-for ip in `cat ips.txt`;do
+sleep 1 & # wait at least one second
+
+# color codes from https://stackoverflow.com/a/5947802
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+ips=(`cat ips.txt`)
+pids=()
+
+
+
+for index in ${!ips[*]}; do
+    ip="${ips[$index]}"
     ./check-router.sh "$ip" &
-    sleep 0.01
+    pids[index]=`$!`
 done
 
-sleep 1 &
+for index in ${!ips[*]}; do
+    ip="${ips[$index]}"
+    pid="${pids[$index]}"
+    wait $pid
+    status="$?"
+    if [ "$status" == "0" ]; then
+        echo -en "${GREEN}$ip${NC} "
+    else
+        echo -en "${RED}$ip${NC} "
+    fi
+done
 
-wait
+wait # wait at least one second
 
 echo
 
