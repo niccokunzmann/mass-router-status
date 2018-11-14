a0a95ab3a70b6b901c5bd74786a0cec38761b462
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-11-14 22:12:12 +0100

    onl remove and update keys if router can be pinged

--- accept-all-ssh-keys.sh
+++ accept-all-ssh-keys.sh
@@ -3,7 +3,10 @@
 cd "`dirname \"$0\"`"
 
 for ip in `cat ips.txt`; do
-    ssh-keygen -R $ip
-    ssh-keyscan $ip 2>>/dev/null >>~/.ssh/known_hosts
+    if ping -c 1 $ip; then
+        echo "updated $ip"
+        ssh-keygen -R $ip
+        ssh-keyscan $ip 2>>/dev/null >>~/.ssh/known_hosts
+    fi
 done
 
